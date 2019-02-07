70f779d746339d3f3da9d6ffa01eca15e65f9e12
arjungurung <gurungar@mnstate.edu>
2019-02-07 13:30:20 -0500

    Properly plotted the predictions and actual data. The program is now able to predict the close amount and describe in a lineplot. Still need to decide on the best way to predict for the newer values.

--- predictor.py
+++ predictor.py
@@ -29,10 +29,7 @@ print(df.head())
 print(df.describe())
 print(df.info())
 '''
-print(df.head(10))
-
-print(df.info())
-
+# Dropped the first row since the date was irregular
 df.drop(df.index[[0, 1]], inplace=True)
 
 '''
@@ -58,9 +55,10 @@ Training the model
 '''Define X and y'''
 X = df[['open', 'volume']]
 y = df['close']
+tempDate = df['date']
 
 # Splitting the train/test data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, shuffle=False, stratify=None)
 
 lm = LinearRegression()
 
@@ -70,13 +68,38 @@ lm.fit(X_train, y_train)
 # Let the model predict the using the training data
 predictions = lm.predict(X_test)
 
+# Check the rms score
 print(np.sqrt(metrics.mean_squared_error(y_test, predictions)))
 
-
 # Check the predictions with the testing data
-sns.set()
-sns.lineplot(data=y_test, palette="tab10", linewidth=2.5)
 
-print(y_test)
-print(predictions)
+tempDate = tempDate.values
+testY = y_test.values
+trainY = y_train.values
+
+totalY = np.concatenate((trainY, testY))
+# totalY = np.flip(totalY)
+totalPred = np.concatenate((trainY, predictions))
+# totalPred = np.flip(totalPred)
+
+
+# Plotting the data to visualize the results
+sns.set()
+plt.figure(figsize=(16, 10))
+
+# sns.lineplot(data=totalPred, palette="tab10", linewidth=2.5)
+# sns.lineplot(data=totalY, palette="tab10", linewidth=2.5)
+
+sns.lineplot(x=tempDate, y=totalPred, palette="tab10", linewidth=2.5, sort=False)
+sns.lineplot(x=tempDate, y=totalY, palette="tab10", linewidth=2.5, sort=False)
+plt.xticks(rotation=90)
+plt.title('AAPL Stock Prediction (P: prediction; r: Actual)')
+plt.legend('Predictions')
 plt.show()
+
+# data = np.array(['172.40', '18260453'])
+# s = pd.Series(data, index=['open', 'volume'])
+# print(s)
+# newClose = lm.predict((s))
+# print(newClose)
+# print(X_test.iloc[0, :])
